45190350b473533785bbf92f56bd6a5c8cfe6c20
김재희 <137701489+jaeheeeKim@users.noreply.github.com>
2023-09-03 21:42:51 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(1<=n && n<=100){
+            if(n%7 == 0){
+                answer = n/7;
+            }else if(n%7 > 0){
+                answer = n/7 + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
