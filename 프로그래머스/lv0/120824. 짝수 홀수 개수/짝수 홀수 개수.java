0d0a3feaaff1719e52b120cef54f5c1e3980d362
김재희 <137701489+jaeheeeKim@users.noreply.github.com>
2023-09-06 22:59:44 +0900

    Update 짝수 홀수 개수.java

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -13,3 +13,15 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 사람의 풀이
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        for(int i = 0; i < num_list.length; i++)
+            answer[num_list[i] % 2]++;
+
+        return answer;
+    }
+}
