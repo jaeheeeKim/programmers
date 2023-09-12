66fdcd66aa2464908c4d200366d45abc1b66da59
김재희 <137701489+jaeheeeKim@users.noreply.github.com>
2023-09-12 23:18:54 +0900

    Update 순서쌍의 개수.java

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -15,3 +15,16 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 사람의 풀이
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
